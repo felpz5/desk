09a8d0858fa5fa5863e4e3942656898046cef3f0
Felipe Gomes <felipegomes0253@gmail.com>
2025-09-11 19:55:38 -0300

    Create bd.py

---
+++ 09_09.py/bd.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+horas_estudadas = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+notas = [3, 2.5, 3, 4, 5, 7, 9, 8.5, 10, 8, 9.5]
+
+plt.scatter(horas_estudadas, notas)
+plt.show
